fa450e553f178bf329c6b455a0500b294e77bef7
Aman Kumar <amankumar@Amans-MacBook-Air.local>
2023-07-29 22:21:12 +0530

    format design

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
-import Homescreen from './screens/Homescreen';
-import Bookingscreen from './screens/Bookingscreen';
-import Loginscreen from './screens/Loginscreen';
-import Registerscreen from './screens/Registerscreen';
-import 'antd/dist/antd.min.css';
-import Profilescreen from './screens/Profilescreen';
-import Adminscreen from './screens/Adminscreen';
-import Landingscreen from './screens/Landingscreen';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Homescreen from "./screens/Homescreen";
+import Bookingscreen from "./screens/Bookingscreen";
+import Loginscreen from "./screens/Loginscreen";
+import Registerscreen from "./screens/Registerscreen";
+import "antd/dist/antd.min.css";
+import Profilescreen from "./screens/Profilescreen";
+import Adminscreen from "./screens/Adminscreen";
+import Landingscreen from "./screens/Landingscreen";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Router> 
+      <Router>
         <Routes>
-          <Route path="/" exact element={<Landingscreen/>}/>
-          <Route path="/home" exact element={<Homescreen/>}/>
-          <Route path="/book/:roomid/:fromdate/:todate" exact element={<Bookingscreen />} />
+          <Route path="/" exact element={<Landingscreen />} />
+          <Route path="/home" exact element={<Homescreen />} />
+          <Route
+            path="/book/:roomid/:fromdate/:todate"
+            exact
+            element={<Bookingscreen />}
+          />
           <Route path="/login" exact element={<Loginscreen />} />
           <Route path="/register" exact element={<Registerscreen />} />
           <Route path="/profile" exact element={<Profilescreen />} />
--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,113 +1,133 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Room from "../components/Room";
-import { DatePicker } from 'antd';
-import 'antd/dist/antd.min.css';
-import moment from 'moment';
+import { DatePicker } from "antd";
+import "antd/dist/antd.min.css";
+import moment from "moment";
 import Loader from "../components/Loader";
 import Error from "../components/Error";
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
 const { RangePicker } = DatePicker;
 
 function Homescreen() {
+  const [rooms, setrooms] = useState([]);
+  const [loading, setloading] = useState([]);
+  const [error, seterror] = useState([]);
 
-  const [rooms, setrooms] = useState([])
-  const [loading, setloading] = useState([])
-  const [error, seterror] = useState([])
+  const [fromdate, setfromdate] = useState();
+  const [todate, settodate] = useState();
+  const [dublicaterooms, setdublicaterooms] = useState([]);
+  const [searchkey, setsearchkey] = useState();
+  const [type, settype] = useState("all");
 
-  const [fromdate, setfromdate] = useState()
-  const [todate, settodate] = useState()
-  const [dublicaterooms, setdublicaterooms] = useState([])
-  const [searchkey, setsearchkey] = useState()
-  const [type, settype] = useState('all')
-  
   const getData = async () => {
     try {
-      setloading(true)
-      const data = (await axios.get('/api/rooms/getallrooms')).data
+      setloading(true);
+      const data = (await axios.get("/api/rooms/getallrooms")).data;
       setrooms(data);
       setdublicaterooms(data);
       setloading(false);
       seterror(false);
-
     } catch (error) {
       seterror(true);
       console.log(error);
       setloading(false);
     }
-  }
+  };
 
   useEffect(() => {
     getData();
-  }, [])
+  }, []);
 
   function filterByDate(dates) {
-
-    setfromdate(moment(dates[0]).format('DD-MM-YYYY'));
-    settodate(moment(dates[1]).format('DD-MM-YYYY'));
+    setfromdate(moment(dates[0]).format("DD-MM-YYYY"));
+    settodate(moment(dates[1]).format("DD-MM-YYYY"));
 
     var temprooms = [];
     var availability = false;
     for (const room of dublicaterooms) {
       if (room.currentbookings.length > 0) {
         for (const booking of room.currentbookings) {
-          if (moment(moment(dates[0]).format('DD-MM-YYYY')).isBetween(booking.fromdate, booking.todate) && moment(moment(dates[1]).format('DD-MM-YYYY')).isBetween(booking.fromdate, booking.todate)) {
+          if (
+            moment(moment(dates[0]).format("DD-MM-YYYY")).isBetween(
+              booking.fromdate,
+              booking.todate
+            ) &&
+            moment(moment(dates[1]).format("DD-MM-YYYY")).isBetween(
+              booking.fromdate,
+              booking.todate
+            )
+          ) {
             if (
-              moment(dates[0].format('DD-MM-YYYY')) !== booking.fromdate &&
-              moment(dates[0].format('DD-MM-YYYY')) !== booking.todate &&
-              moment(dates[1].format('DD-MM-YYYY')) !== booking.fromdate &&
-              moment(dates[1].format('DD-MM-YYYY')) !== booking.todate){
-                   availability=true;     
-              }
+              moment(dates[0].format("DD-MM-YYYY")) !== booking.fromdate &&
+              moment(dates[0].format("DD-MM-YYYY")) !== booking.todate &&
+              moment(dates[1].format("DD-MM-YYYY")) !== booking.fromdate &&
+              moment(dates[1].format("DD-MM-YYYY")) !== booking.todate
+            ) {
+              availability = true;
             }
+          }
         }
       }
 
-      if(availability== true || room.currentbookings.length==0)
-      {
-        temprooms.push(room)
+      if (availability == true || room.currentbookings.length == 0) {
+        temprooms.push(room);
       }
 
       setrooms(temprooms);
     }
   }
-  
 
-  function filterBySearch(){
-    const temprooms = dublicaterooms.filter(room=>room.name.toLowerCase().includes(searchkey.toLowerCase()))
+  function filterBySearch() {
+    const temprooms = dublicaterooms.filter((room) =>
+      room.name.toLowerCase().includes(searchkey.toLowerCase())
+    );
 
     setrooms(temprooms);
   }
 
   function filterByType(e) {
     settype(e);
-    
-    if(e!=='all')
-    {
-      const temprooms = dublicaterooms.filter(room => room.type.toLowerCase()==e.toLowerCase())
-     
+
+    if (e !== "all") {
+      const temprooms = dublicaterooms.filter(
+        (room) => room.type.toLowerCase() == e.toLowerCase()
+      );
+
       setrooms(temprooms);
-    }
-    else{
+    } else {
       setrooms(dublicaterooms);
-    }  
+    }
   }
   return (
     <div className="container">
       <div className="row mt-5 bs filter">
         <div className="col-md-3">
-          <RangePicker format='DD-MM-YYYY' onChange={filterByDate} />
+          <RangePicker format="DD-MM-YYYY" onChange={filterByDate} />
         </div>
         <div className="col-md-3">
-          <input type='text' className='form-control' placeholder='Search Rooms' value={searchkey} onChange={(e)=>{setsearchkey(e.target.value)}}
-          onKeyUp={filterBySearch}
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search Rooms"
+            value={searchkey}
+            onChange={(e) => {
+              setsearchkey(e.target.value);
+            }}
+            onKeyUp={filterBySearch}
           />
         </div>
         <div className="col-md-3">
-          <Form.Select aria-label="Default select example" value={type} onChange={(e)=>{filterByType(e.target.value)}}>
-            <option value='all'>All</option>
-            <option value='delux'>Delux</option>
-            <option value='non-delux'>Non-Delux</option>
+          <Form.Select
+            aria-label="Default select example"
+            value={type}
+            onChange={(e) => {
+              filterByType(e.target.value);
+            }}
+          >
+            <option value="all">All</option>
+            <option value="delux">Delux</option>
+            <option value="non-delux">Non-Delux</option>
           </Form.Select>
         </div>
       </div>
@@ -116,14 +136,16 @@ function Homescreen() {
           <Loader />
         ) : (
           rooms.map((room) => {
-            return <div className="col-md-9 mt-4">
-              <Room room={room} fromdate={fromdate} todate={todate} />
-            </div>;
+            return (
+              <div className="col-md-9 mt-4">
+                <Room room={room} fromdate={fromdate} todate={todate} />
+              </div>
+            );
           })
         )}
       </div>
     </div>
-  )
+  );
 }
 
-export default Homescreen
+export default Homescreen;
