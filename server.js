06eb952b43da33ec7847ceed3a5f576513e3b9fe
Aman Kumar <apcode15@gmail.com>
2023-08-04 17:28:00 +0530

    cors

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const dbConfig = require('./db');
-const roomsRoute = require('./routes/roomsRoute')
-const usersRoute = require('./routes/usersRoute')
-const bookingsRoute =require('./routes/bookingsRoute')
-app.use(express.json())
+const dbConfig = require("./db");
+const roomsRoute = require("./routes/roomsRoute");
+const usersRoute = require("./routes/usersRoute");
+const bookingsRoute = require("./routes/bookingsRoute");
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/rooms', roomsRoute)
-app.use('/api/users', usersRoute)
-app.use('/api/bookings', bookingsRoute) 
+app.use("/api/rooms", roomsRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/bookings", bookingsRoute);
 
 const port = process.env.PORT || 5000;
-app.get('/',(req, res)=>{
-    res.send(`I'm ON`);
+
+app.get("/", async (req, res) => {
+  res.send(`I'm ON`);
 });
 
 app.listen(port, () => {
-    console.log('Server is started with nodemon on:',port)
+  console.log("Server is started with nodemon on:", port);
 });
